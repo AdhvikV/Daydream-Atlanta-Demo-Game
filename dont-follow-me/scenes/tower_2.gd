4fe85c7c7b8fcf872780224eb309f8760d01182f
AdhvikV <adhvikcow@gmail.com>
2025-09-28 17:31:07 -0400

    e

--- dont-follow-me/scenes/tower_2.gd
+++ dont-follow-me/scenes/tower_2.gd
@@ -1,1 +1,9 @@
-extends Node2D
+extends Control
+
+@onready var button = $Button  # Your button node
+
+func _ready():
+	button.pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/tower_1.tscn")
